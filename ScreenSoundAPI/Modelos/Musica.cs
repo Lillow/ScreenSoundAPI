fe641ec7b2224ead1374b232e576da8d8f1660ad
Danillo <danillordm@gmail.com>
2024-04-22 12:40:30 -0300

    first commit

---
+++ ScreenSoundAPI/Modelos/Musica.cs
@@ -0,0 +1,27 @@
+﻿using System.Text.Json.Serialization;
+
+namespace ScreenSoundAPI.Modelos
+{
+    internal class Musica
+    {
+        [JsonPropertyName("song")]
+        public string? Nome { get; set; }
+
+        [JsonPropertyName("artist")]
+        public string? Artista { get; set; }
+
+        [JsonPropertyName("duration_ms")]
+        public int? Duracao { get; set; }
+
+        [JsonPropertyName("genre")]
+        public string? Genero { get; set; }
+
+        public void ExibirDetalhes()
+        {
+            Console.WriteLine($"Música: {Nome}\nArtista: {Artista}\nDuração: {Duracao / 1000}seg\nGénero: {Genero}");
+        }
+
+
+    }
+
+}
---
+++ ScreenSoundAPI/Program.cs
@@ -0,0 +1,24 @@
+﻿using ScreenSoundAPI.Modelos;
+using System.Text.Json;
+
+using (HttpClient Client = new())
+{
+
+
+    try
+    {
+        var resposta = await Client.GetStringAsync("https://guilhermeonrails.github.io/api-csharp-songs/songs.json");
+        var musicas = JsonSerializer.Deserialize<List<Musica>>(resposta)!;
+        // musicas[1998].ExibirDetalhes();
+        //LinqFilter.ExibirListaDeArtistasOrdenados(musicas);
+        //LinqFilter.FiltrarTodosOsGenerosMusicais(musicas);
+        //LinqFilter.FiltrarArtistasPorGeneroMusical(musicas, "rock");
+        LinqFilter.FiltrarMusicasDeUmArtista(musicas, "Michel Teló");
+
+    }
+    catch (Exception ex)
+    {
+        Console.WriteLine($"Temos um problema: {ex.Message}");
+    }
+
+}
